ce85a33f3eb9d96337b911b84b213a11a30d9bd7
Wang Wenda <wwd7086@gmail.com>
2014-11-20 14:11:27 -0500

    add something

--- setup.sh
+++ setup.sh
@@ -4,7 +4,13 @@ sudo apt-get install lxde
 echo 'install the qt5'
 sudo apt-get install qt5-default
 
+#install the OpenCV
+
 #install the callibrator
 
 
 #setting up the network
+#edit the interface file
+
+#setting up the serial port
+#copy the dtbo file
